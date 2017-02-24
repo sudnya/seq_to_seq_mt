7dc2626f64e883dcb307eb427c4d68a7b9dc22a8
sl <siyunlai@gmail.com>
2017-02-24 14:41:58 -0800

    fixed

--- encoder.py
+++ encoder.py
@@ -14,7 +14,7 @@ def add_embedding(model, inputs):
     """
     config = model.config
 
-    with tf.variable_scope('EmbeddingLayer'):
+    with tf.variable_scope('EncodingEmbeddingLayer'):
         w2v = tf.get_variable('w2v', [model.en_vocab_size, config.hidden_size], initializer=xavier_init)
         output = tf.nn.embedding_lookup(params=w2v, ids=inputs)
         output = tf.split(output, tf.ones(config.en_num_steps, dtype=tf.int32), axis=1)
