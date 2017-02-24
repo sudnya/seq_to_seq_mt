ba07fcc9ad984f6e178abcd53aa10594f1520bf9
sudnya <mailsudnya@gmail.com>
2017-02-24 11:33:28 -0800

    ENH: added data iterator to co iterate over enc and dec

--- data_iterator.py
+++ data_iterator.py
@@ -1,24 +1,34 @@
 import numpy as np
 
 
-def data_iterator(en_data, de_data, batch_size, dtype=np.int32):
-    # Pulled from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/rnn/ptb/reader.py#L82
+def data_iterator(en_data, de_data, batch_size, num_steps, dtype=np.int32):
     en_data = np.array(en_data, dtype=dtype)
     de_data = np.array(de_data, dtype=dtype)
+    
     data_len = len(en_data)
 
     assert data_len == len(de_data), 'encoder data length does not match decoder data length'
+    
+    len_per_batch = data_len // batch_size
+
+    t_en_data = np.zeroes([batch_size, len_per_batch], dtype=dtype)
+    t_de_data = np.zeroes([batch_size, len_per_batch], dtype=dtype)
 
 
-    data_len = len(full_data)
-    batch_len = data_len // batch_size
-    data = np.zeros([batch_size, batch_len], dtype=dtype)
     for i in range(batch_size):
-        data[i] = full_data[batch_len * i:batch_len * (i + 1)]
-    epoch_size = (batch_len - 1) // num_steps
-    if epoch_size == 0:
-        raise ValueError("epoch_size == 0, decrease batch_size or num_steps")
-    for i in range(epoch_size):
-        x = data[:, i * num_steps:(i + 1) * num_steps]
-        y = data[:, i * num_steps + 1:(i + 1) * num_steps + 1]
-        yield (x, y)
+        t_en_data[i] = en_data[len_per_batch * i : len_per_batch * (i+1)]
+        t_de_data[i] = de_data[len_per_batch * i : len_per_batch * (i+1)]
+
+    # we need to run at least 1 epoch and eg: 32 is len of sentence per batch and recurrence
+    # length i.e. time steps in RNN is 40 then we are in trouble since we will not have 
+    # sufficient length of words to go over
+    epochs = (len_per_batch - 1) // num_steps
+
+    assert epochs != 0 , "epochs == 0, decrease batch_size or num_steps"
+
+    # co iterate over encoder and decoder over stride of RNN length
+    for i in range(epochs):
+        x = t_en_data[:, i * num_steps : (i+1)*num_steps]
+        y = t_de_data[:, i * num_steps : (i+1)*num_steps]
+        yield(x,y)
+
